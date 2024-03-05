7f69a263bab12584973790b3ff27a7adec12e635
chiyoi <chiyoi2140@outlook.com>
2024-03-05 14:41:43 +0800

    tidy

--- internal/fonts.ts
+++ internal/fonts.ts
@@ -1,11 +1,4 @@
-import { Hachi_Maru_Pop, Noto_Sans_Mono } from 'next/font/google'
-
-const HachiMaruPop = Hachi_Maru_Pop({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: '400',
-})
-export const FontHachiMaruPop = HachiMaruPop.style
+import { Noto_Sans_Mono } from 'next/font/google'
 
 const NotoSansMono = Noto_Sans_Mono({
   subsets: ['latin'],
